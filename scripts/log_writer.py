9a65b7d7e99107f4508f503d40cee94238a7a674
michaelgetachew <mikygetyos@gmail.com>
2022-09-20 03:56:33 +0300

    Feat(add): added the normal logger file

---
+++ scripts/log_writer.py
@@ -0,0 +1,26 @@
+"""This script writes log messages regarding the file it is called upon!!!"""
+import logging
+
+import formatter
+
+class log:
+    "log class"
+
+    def __init__(self, file_name: str, basic_level = logging.INFO):
+        """Constructor: upon call initializes the logger class with the filename and log level"""
+        
+        logger = logging.getLogger(__name__)
+
+        #set log level to INFO
+        logger.setLevel(basic_level)
+
+        file_handler = logging.FileHandler(f"../logs/{file_name}")
+        fomatter = logging.Formatter("%(asctime)s : %(levelname)s: %(name)s: %(module)s: %(funcName)s: %(message)s")
+
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+        self.logger = logger
+
+    def get_app_logger(self) -> logging.log:
+        return self.logger

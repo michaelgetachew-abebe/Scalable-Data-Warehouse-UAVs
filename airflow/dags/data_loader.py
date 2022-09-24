a6c5bebb847719ea58c9698f8c47163caba3cd9e
michaelgetachew <mikygetyos@gmail.com>
2022-09-24 11:25:49 +0300

    Feat(add): added the dbt_dag

--- airflow/dags/data_loader.py
+++ airflow/dags/data_loader.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import dvc.api
 from datetime import datetime
 from datetime import timedelta
 from airflow import DAG
@@ -7,10 +8,12 @@ from airflow.operators.python import PythonOperator
 
 cwd=os.getcwd()
 sys.path.append(f'../scripts/')
+sys.path.append(f'../sql/')
 sys.path.append(f'../temp_storage/')
 sys.path.insert(0,os.path.abspath(os.path.dirname(__file__)))
-from extractor import DataExtractor
-import db_util
+
+from extract import DataExtractor
+import util
 
 data_extractor=DataExtractor()
 
@@ -19,21 +22,22 @@ def extract_data(ti):
     data_url = 'data/location1.csv'
     repo = 'https://github.com/michaelgetachew-abebe/Scalable-Data-Warehouse-UAVs'
     version = 'dcfc7e4f35ea38c7ae9444cc1826e300269f7bb5'
-    loaded_df_name=data_extractor.extract_data(file_name='20181024_d1_0830_0900.csv',return_json=True)
+    data = dvc.api.get_url(path=data_url,repo=repo,rev=version)
+    loaded_df_name=data_extractor.extract_data(file_name=data,return_json=True)
     trajectory_file_name,vehicle_file_name=loaded_df_name
    
     ti.xcom_push(key="trajectory",value=trajectory_file_name)
     ti.xcom_push(key="vehicle",value=vehicle_file_name)
 
 def create_table():
-    db_util.create_table()
+    util.create_table()
 
 def populate_table(ti):
     trajectory_file_name = ti.xcom_pull(key="trajectory",task_ids='extract_from_file')
     vehicle_file_name = ti.xcom_pull(key="vehicle",task_ids='extract_from_file')
     # trajectory_data,vehicle_data=combined_df['trajectory'], combined_df['vehicle']
-    db_util.insert_to_table(trajectory_file_name, 'trajectories',from_file=True)
-    db_util.insert_to_table(vehicle_file_name, 'vehicles',from_file=True)
+    util.insert_to_table(trajectory_file_name, 'trajectories',from_file=True)
+    util.insert_to_table(vehicle_file_name, 'vehicles',from_file=True)
 
 def clear_memory(ti):
     trajectory_file_name = ti.xcom_pull(key="trajectory",task_ids='extract_from_file')
